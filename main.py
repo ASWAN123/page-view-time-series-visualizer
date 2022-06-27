d82a2f12eae7efd240fab254ca77895374dd1da6
aswanaswan <>
2022-06-27 10:58:17 +0000

    27 jun

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from unittest import main
 
 # Test your function by calling it here
 time_series_visualizer.draw_line_plot()
-# time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_bar_plot()
 # time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -9,7 +9,7 @@ df = pd.read_csv('fcc-forum-pageviews.csv'  , parse_dates=['date'], index_col=['
 
 # Clean data
 df =  df.loc[(df["value"] >= df["value"].quantile(0.025))& (df["value"] <= df["value"].quantile(0.975))]
-
+ 
 
 def draw_line_plot():
     # Draw line plot
@@ -29,10 +29,19 @@ def draw_line_plot():
     return fig
 
 def draw_bar_plot():
+    df['Date']= df.index.month
+    df['Years'] =df.index.year
+    df['Months'] = df.index.month_name()
     # Copy and modify data for monthly bar plot
-    df_bar = None
-
+    df_bar = df.groupby([ 'Date' ,'Years' , 'Months'  ])['value'].agg(['mean']).reset_index()
     # Draw bar plot
+    fig, ax = plt.subplots()
+    ax = sns.barplot(x="Years", y="mean", hue= 'Months' , data=df_bar )
+    ax.set(
+        xlabel="Years",
+        ylabel="Average Page Views",
+        title ='Months'
+    )
 
 
 
@@ -41,6 +50,7 @@ def draw_bar_plot():
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
